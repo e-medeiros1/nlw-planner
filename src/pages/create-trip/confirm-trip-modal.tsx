7b4eb118cb46165f5ae3a4905d6d0cd837727893
Erilândio Santos Medeiros <hsymetry@gmail.com>
2024-07-18 22:25:25 -0300

    Fix displayed date in modal

--- src/pages/create-trip/confirm-trip-modal.tsx
+++ src/pages/create-trip/confirm-trip-modal.tsx
@@ -1,6 +1,8 @@
 import { AtSign, Plus, User, X } from "lucide-react";
 import { FormEvent } from "react";
 import { Button } from "../components/button";
+import { DateRange } from "react-day-picker";
+import { format } from "date-fns";
 
 interface ConfirmTripModalProps {
   changeConfirmTripModal: () => void;
@@ -8,6 +10,7 @@ interface ConfirmTripModalProps {
   setOwnerName: (ownerName: string) => void;
   setOwnerEmail: (ownerEmail: string) => void;
   destination: string;
+  eventStartAndEndDates: DateRange | undefined;
 }
 
 export function ConfirmTripModal({
@@ -16,7 +19,19 @@ export function ConfirmTripModal({
   setOwnerEmail,
   setOwnerName,
   destination,
+  eventStartAndEndDates,
 }: ConfirmTripModalProps) {
+
+  const displayedDate =
+    eventStartAndEndDates &&
+    eventStartAndEndDates.from &&
+    eventStartAndEndDates.to
+      ? format(eventStartAndEndDates.from, "d' de 'LLL'")
+          .concat(" até ")
+          .concat(format(eventStartAndEndDates.to, "d' de 'LLL"))
+      : null;
+
+
   return (
     <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
       <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
@@ -38,7 +53,7 @@ export function ConfirmTripModal({
             <span className="font-semibold text-zinc-100">{destination}</span>{" "}
             nas datas de{" "}
             <span className="font-semibold text-zinc-100">
-              16 a 27 de Agosto de 2024
+              {displayedDate}
             </span>{" "}
             preencha seus dados abaixo:
           </p>
